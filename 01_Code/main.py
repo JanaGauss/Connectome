75010e760f5dee341f3685d7c2f2bf004ceeaa06
Katharina Julia Brenner <katharina.brenner@cdtm.de>
2021-11-18 14:52:33 +0100

    setup folder structure v2

--- 01_Code/main.py
+++ 01_Code/main.py
@@ -1,44 +1,14 @@
-import mat73
-import matplotlib.pyplot as plt
-import seaborn as sns
+"""
+execution file to run complete code
+Author:
+Created:
+"""
 
 
-data_path = "C:/Users/katha/Downloads/SBC_01/"
+# run preprocessing
 
-global_connectivity = []
-for i in range(1, 813):
+# run model x
 
-    if i < 10:
-        filenumber = f"00{i}"
-    elif i < 100:
-        filenumber = f"0{i}"
+# run model y
 
-    else:
-        filenumber = i
-
-    datafile_path = data_path + f"resultsROI_Subject{filenumber}_Condition001.mat"
-
-    data_dict = mat73.loadmat(datafile_path)
-
-    # data_dict.keys()
-
-    connectivity_matrix = data_dict["Z"]
-    # .append zum erstellen von listen
-    global_connectivity.append(connectivity_matrix)
-
-
-
-    fig1 = plt.figure(figsize = (6.5, 4.5))  #TODO: figuresize
-    sns.heatmap(connectivity_matrix)
-
-    # plt.show()
-    # Achtung: Zukünftig nur plt.savefig"test.svg" (selektieren), da das folgende alles abspeichert
-
-    # plt.savefig(f"plots/heatmap{i}.svg")
-    print("end")
-
-for i in range(len(global_connectivity)):
-
-    print(global_connectivity[i][1,0])
-    # 1.4306549437823974
-    # 0.7001152237820871 für erste und zweite iteration
+# visualize results / postprocessing
