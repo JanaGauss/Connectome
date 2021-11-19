c7ea87d2512a0d62b643cb028dd7649e3a217712
Jana Gauß <ja.gauss@t-online.de>
2021-11-19 18:59:18 +0100

    elastic net

--- 01_Code/02_model/elastic_net.R
+++ 01_Code/02_model/elastic_net.R
@@ -2,7 +2,7 @@
 # first try
 
 # https://glmnet.stanford.edu/articles/glmnet.html
-library(glmnet)
+
 library(dplyr)
 
 devtools::install("01_Code/ConnectomeR") # only necessary if there are changes
@@ -11,25 +11,39 @@ library(ConnectomeR)
 data <- readRDS("00_Data/Delcode_prepared_2021-11-19.rds")
 table(data$prmdiag)
 
-# first model: healthy controls (0) vs MCI and AD (2, 3)
-data <- data %>%
-  mutate(y = case_when(
-  data$prmdiag == "0" ~ 0,
-  data$prmdiag %in% c("2", "3") ~ 1,
-  TRUE ~ NA_real_
-  )) %>%
-  filter(!is.na(y))
 
-table(data$y)
 
-model_1 <- glmnet(x = data[, 13:(ncol(data) - 1)], # ToDo: extract columns with connectivity matrix data automatically
-                  y = data$y,
-                  family = "binomial",
-                  alpha = 1 # Lasso
-                  )
-print(model_1)
-plot(model_1)
-coef(model_1, s = 0.1)
 
-predict(model_1, newx = as.matrix(data[, 13:(ncol(data) - 1)]), s = model_1$a0, type = "response")
+# try preprocessing functions
+vars <- names_conn(colnames(data))
+d2 <- prep_y(data, y_0 = c("0"), y_1 = c("2", "3"))
+table(data$prmdiag)
+table(d2$y)
+
+ids <- sample(d2$IDs, size = 470/2)
+
+data_list <- train_test_data(d2, train_IDs = ids)
+train <- data_list[["train"]]
+test <- data_list[["test"]]
+
+# try model
+model <- glmnet(x = train[, vars], y = train$y, family = "binomial", alpha = 0)
+
+pred <- predict(model, newx = as.matrix(test[, vars]), type = "response")
+acc <- calc_acc_elnet(pred, test$y)
+
+test_1 <- calc_el_net(train, test, vars, alpha = 0, metric = "accuracy")
+test_2 <- calc_el_net(train, test, vars, alpha = 1, metric = "accuracy", nlambda = 200)
+test_1$metric_value
+test_2$metric_value
+
+# function doing all that stuff at once:
+elastic_net <- el_net(data, train_IDs = ids, alpha = c(0, 1), y_0 = c("0"), y_1 = c("2", "3"))
+elastic_net$metric_values
+
+elastic_net_2 <- el_net(data, train_IDs = ids, y_0 = c("0"), y_1 = c("2", "3")) # for alpha = seq(0, 1, by = 0.1)
+elastic_net_2$metric_values
+# -> Lasso (alpha = 0) works best
+elnet_best <- elastic_net_2$results_models[[1]]
+
 
--- 01_Code/ConnectomeR/R/models.R
+++ 01_Code/ConnectomeR/R/models.R
@@ -1,11 +1,13 @@
 # R functions for models
 
-#' elastic net model
+#' elastic net model 
 #'
+#' calculates elastic net model for several alpha values and chooses best model according to some metric
 #' @param data dataset
 #' @param vars which variables should be used? If null, columns containing connectivity data are automatically extracted
-#' @param test_IDs IDs for test data
-#' @param alpha alpha value for glmnet. 0 = Lasso, 1 = Ridge
+#' @param train_IDs IDs for training data
+#' @param test_IDs IDs for test data. If null, use all data that is not in training data
+#' @param alpha alpha values for glmnet. 0 = Lasso, 1 = Ridge
 #' @param y_0 which levels of data$prmdiag use as 0
 #' @param y_1 which levels of data$prmdiag use as 1
 #' @param data_source which dataset is it? (in case there have to be made adjustments)
@@ -13,64 +15,119 @@
 #' @param ... Additional parameters for glmnet function, e.g. nlambda
 #' @import dplyr glmnet checkmate
 #' @export
-el_net <- function(data, vars = NULL, test_IDs, alpha, y_0, y_1, metric = "accuracy", ...){
-  
-  
+el_net <- function(data, vars = NULL, train_IDs, test_IDs = NULL, alpha = seq(0, 1, by = 0.1), y_0, y_1, 
+                   data_source = "DELCODE", metric = "accuracy", ...){
+
+
   checkmate::assert_data_frame(data)
   checkmate::assert_character(vars, null.ok = TRUE)
   checkmate::check_integer(test_IDs)
-  checkmate::assert_number(alpha, lower = 0, upper = 1)
-  checkmate::assert_true(y_0 %in% levels(data$prmdiag))
-  checkmate::assert_true(y_1 %in% levels(data$prmdiag))
+  checkmate::assert_numeric(alpha, lower = 0, upper = 1)
+  checkmate::assert_true(all(y_0 %in% levels(data$prmdiag)))
+  checkmate::assert_true(all(y_1 %in% levels(data$prmdiag)))
   checkmate::assert_choice(metric, choices = c("accuracy")) # ToDo: add more metrics
+  checkmate::assert_choice(data_source, choices = c("DELCODE"))
+  
   if(!is.null(vars)){
     checkmate::assert_true(vars %in% colnames(data))
-  } else{ 
+  } else{
     # extract all columns with connectivity data automatically
     vars <- names_conn(colnames(data))
   }
-  
+
   # prepare data
+  data <- prep_y(data, y_0, y_1)
   
-  data <- data %>%
-    mutate(y = case_when(
-      data$prmdiag %in% y_0 ~ 0,
-      data$prmdiag %in% y_1 ~ 1,
-      TRUE ~ NA_real_
-    )) %>%
-    filter(!is.na(y)) %>%
-    select(IDs, y, vars)
-    
-  data_test <- data %>% filter(IDs %in% test_IDs)
-  data_train <- setdiff(data, data_test)
-  
-  # calculate model (for several lambda values)
-  model <- glmnet(x = data_train[, vars], y = data_train$y, family = "binomial", alpha = alpha, ...)
-  
-  # find best model according to metric
-  # 1. predict fitted values on test data for all lambda
-  pred_test <- predict(model, newx = as.matrix(data_test[, vars]), s = model$a0, # for all lambda values
-                       type = "response")
-  
-  # 2. calculate metric for all lambda
-  if(metric == "accuracy"){
-    
+  data_list <- train_test_data(data, train_IDs, test_IDs)
+  train <- data_list[["train"]]
+  test <- data_list[["test"]]
+
+  # calculate elastic net for all alpha values
+  results <- list()
+  metric_values <- rep(0, length(alpha))
+  for(a in 1:length(alpha)){
+    results[[a]] <- calc_el_net(train, test, vars, alpha = alpha[a], metric, ...)
+    metric_values[a] <- results[[a]][["metric_value"]]
   }
+  
+  res <- list(results, metric_values)
+  names(res) <- c("results_models", "metric_values")
+  return(res)
   
   
 }
 
-#' extract names of connectivity variables
+
+#' elastic net model for one alpha
 #'
-#' @param colnames colnames of dataset
-#' @import dplyr stringr checkmate
+#' calculates elastic net model for one alpha value and chooses best model according to some metric (returns list with information)
+#' @param data_train training data
+#' @param data_test test data
+#' @param vars which variables should be used? If null, columns containing connectivity data are automatically extracted
+#' @param alpha alpha value for glmnet. 0 = Lasso, 1 = Ridge
+#' @param metric which metric is used to find the best lambda? !!! ToDo: add more metrics, think about advantages/disadvantages
+#' @param ... Additional parameters for glmnet function, e.g. nlambda
+#' @import dplyr glmnet checkmate
 #' @export
-names_conn <- function(colnames){
+calc_el_net <- function(data_train, data_test, vars, alpha = 0, metric = "accuracy", ...){
   
-  checkmate::assert_character(colnames)
+  checkmate::assert_data_frame(data_train)
+  checkmate::assert_data_frame(data_test)
+  checkmate::assert_character(vars)
+  checkmate::assert_number(alpha, lower = 0, upper = 1)
+  checkmate::assert_choice(metric, choices = c("accuracy")) # ToDo: add more metrics
   
-  cols <- str_detect(colnames, "\\d+_\\d+") # search for one or more digits, _, one or more digits
-  vars <- colnames[cols]
-  return(vars)
+  model <- glmnet(x = data_train[, vars], y = data_train$y, family = "binomial", alpha = alpha, ...)
+  
+  pred <- predict(model, newx = as.matrix(data_test[, vars]), type = "response") # predictions on test data for all lambda
+  
+  if(metric == "accuracy"){
+    acc <- calc_acc_elnet(pred, data_test$y)
+    ind_best_lambda <- which(acc == max(acc))[1] # [1] -> take only one lambda if there several best ones
+    
+    value_metric <- max(acc)
+    
+  } else{
+    # ToDo
+  }
+  
+  # return information of best model
+  result <- list()
+  result[["model"]] <- model
+  result[["alpha"]] <- alpha
+  result[["a0"]] <- model$a0[ind_best_lambda] # Intercept
+  result[["beta"]] <- model$beta[, ind_best_lambda]
+  result[["ind_lambda"]] <- ind_best_lambda
+  result[["lambda"]] <- model$lambda[ind_best_lambda]
+  result[["metric_value"]] <- value_metric
+  
+  return(result)
+
+}
+
+
+
+#' calculate accuracy for one elastic net model
+#'
+#' calculates accuracy for one elastic net model for several lambda values, returns accuracy for every lambda
+#' @param pred matrix with predictions
+#' @param y real y of predicitons
+#' @import dplyr caret checkmate
+#' @export
+calc_acc_elnet <- function(pred, y){
+  
+  checkmate::assert_matrix(pred)
+  checkmate::assert_numeric(y)
+  checkmate::assert_true(nrow(pred) == length(y))
+  
+  pred_01 <- matrix(as.integer(pred >= 0.5), byrow = FALSE, nrow = nrow(pred))
+  y <- factor(y)
+  
+  acc <- rep(0, ncol(pred))
+  for(i in 1:ncol(pred)){ # calculate accuracy for each lambda value (=each column of prediction matrix)
+    acc[i] <- confusionMatrix(factor(pred_01[, i], levels = c("0", "1")), y)$overall["Accuracy"]
+  }
+  
+  return(acc)
 }
 
