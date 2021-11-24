a565f41f749a788ee583a4b42e598c5f80852c7d
leoschaabner <73901213+leoschaabner@users.noreply.github.com>
2021-11-24 20:51:08 +0100

    #first few unittest for preprocessing + updates on preprocessing file

--- 01_Code/01_preprocessing/test_matlab_to_hdf.py
+++ 01_Code/01_preprocessing/test_matlab_to_hdf.py
@@ -6,18 +6,34 @@ import pandas as pd
 
 
 class TestMatlabToHdf(unittest.TestCase):
+    d = {'col1': [1, 2, 3], 'col2': [4, 5, 6], 'col3': [7, 8, 9]}
+    excel_data = pd.DataFrame(data=d)
+
+    file_list = ["resultsROI_Subject012_Condition001.mat",
+                 "resultsROI_Subject029_Condition001.mat",
+                 "resultsROI_Subject090_Condition001"]
+
+    stacked_matrices = np.array([[1, 2, 1],
+                                 [2, 2, 0],
+                                 [2, 1, 3]])
+
+    target_d = {"col1": [1, 2, 3], "col2": [4, 5, 6], "col3": [7, 8, 9],
+                "IDs": [12, 29, 90], "1_2": [1, 2, 2], "1_3": [2, 2, 1], "2_3": [1, 0, 3]}
+    target_df = pd.DataFrame(data=target_d)
+
+    target_colnames = ['col1', 'col2', 'col3', 'IDs', '1_2', '1_3', '2_3']
+
     def test_write_to_dir(self):
+
         with self.assertRaises(FileNotFoundError):
             mth.write_to_dir([], "no valid directory", "csv")
 
         self.assertEqual(mth.write_to_dir([], os.getcwd(), "excel"), "invalid file format selected")
 
     def test_get_subject_ids(self):
-        file_list = ["resultsROI_Subject012_Condition001.mat",
-                     "resultsROI_Subject029_Condition001.mat",
-                     "resultsROI_Subject090_Condition001"]
+
         target_result = np.array([12, 29, 90])
-        result = mth.get_subject_ids(file_list)
+        result = mth.get_subject_ids(self.__class__.file_list)
         array_eq = (target_result == result).all()
 
         self.assertEqual(array_eq, True)
@@ -29,25 +45,21 @@ class TestMatlabToHdf(unittest.TestCase):
 #    def test_create_train_test_split(self):
 #        pass
 
-    def test_col_names_conn_matrix(self):
-        d = {'col1': [1, 2], 'col2': [3, 4], 'col3': [5, 6]}
-        excel_data = pd.DataFrame(data=d)
-        test_colnames = mth.col_names_final_df(excel_data, 3)
-        target_colnames = ['col1', 'col2', 'col3', 'IDs', '1_2', '1_3', '2_3']
-
-        array_eq2 = (np.array(target_colnames) == np.array(test_colnames)).all()
-
-        self.assertEqual(array_eq2, True)
-
     def test_create_final_df(self):
+        result_df = mth.create_final_df(self.__class__.file_list,
+                                        self.__class__.target_colnames,
+                                        self.__class__.stacked_matrices,
+                                        self.__class__.excel_data)
+        df_eq = all(result_df == self.__class__.target_df)
+        self.assertEqual(df_eq, True)
+
+    def test_col_names_conn_matrix(self):
         pass
 
-
-
-#    def test_col_names_final_df(self):
+#    def test_flatten_conn_matrix(self):
 #        pass
 
-#    def test_flatten_conn_matrix(self):
+#    def test_col_names_final_df(self):
 #        pass
 
 #    def test_stack_matrices(self):
