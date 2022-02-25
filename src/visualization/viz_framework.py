3e24dca349fb8659822b4020a7f82232bd112355
JanaGauss <50174513+JanaGauss@users.noreply.github.com>
2022-02-25 12:50:18 +0100

    small change viz_framework
    m (number of permutations) can be specified via **kwargs, default of grouped feature imp. functions will be changed to m=5

--- src/visualization/viz_framework.py
+++ src/visualization/viz_framework.py
@@ -34,14 +34,14 @@ def visualization_framework(model,
 	# groups_df = pd.read_csv("references/colnames_to_yeo7.csv")
 	groups_df = get_colnames_df()
 	groups_df = groups_df.loc[np.in1d(groups_df["conn_name"], X.columns)] # remove entries of groups_df that are not in colnames of X
-	df_importance = grouped_permutation_FI(model, X, y, groups_df, m = 5)
+	df_importance = grouped_permutation_FI(model, X, y, groups_df)
 	return plot_grouped_FI(df_importance)
         
     elif viz_method == "GFI_only":
 	# groups_df = pd.read_csv("references/colnames_to_yeo7.csv")
 	groups_df = get_colnames_df()
 	groups_df = groups_df.loc[np.in1d(groups_df["conn_name"], X.columns)] # remove entries of groups_df that are not in colnames of X
-	df_importance = group_only_permutation_FI(model, X, y, groups_df, m = 5)
+	df_importance = group_only_permutation_FI(model, X, y, groups_df)
 	return plot_grouped_FI(df_importance)
 
     #Elastic Net
