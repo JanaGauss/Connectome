89bf0136a891149b6832ed055f0776af0afdf13b
JonasKlingele <93312483+JonasKlingele@users.noreply.github.com>
2022-03-01 12:11:15 +0100

    Updated function calls and fixed errors

--- src/models/framework.py
+++ src/models/framework.py
@@ -3,10 +3,14 @@ import keras
 import tensorflow as tf
 from keras.models import load_model
 import pickle
+# from src.models.brainnet_cnn import model_brainnet_cnn
+from src.models.ebm import EBMmi
+from src.models.lgb import GB
 
 from src.models.pipeline_elastic_net import model_elastic_net
 
-def model_framework(X_train, y_train, #Brauchen wir die 2 für pretrained models?
+
+def model_framework(X_train, y_train,  # Brauchen wir die 2 für pretrained models?
                     model: str,
                     pretrained: bool = True,
                     model_path: str = None,
@@ -19,64 +23,62 @@ def model_framework(X_train, y_train, #Brauchen wir die 2 für pretrained models
         pretrained: If a new model should be trained or a pretrained model should be used
                     (True/False, default True uses pretrained model
         model: A string to enter which model to use ("elnet" = elastic net, "gboost" = gradient boosting,
-                                                     "rf" = random forest, "cnn" = convolutional neural network)
+                                                     "rf" = random forest, "cnn" = convolutional neural network,
+                                                     "ebm" = explainable boosting machine)
         model_path: Full path to the folder where the selected pretrained model is stored
         **kwargs: Additional parameters and options depending on the selected model
     Returns:
         A fitted model
 
-    Raises: 
+    Raises:
         FileNotFoundError
         IOError
     """
-    #to do: gboost, rf, pretrained models aufrufen? assert pd.dataframe?
+    # to do: gboost, rf, pretrained models aufrufen? assert pd.dataframe?
     assert isinstance(model, str), "invalid option, must be string"
-    assert model in ["elnet", "gboost", "rf","cnn"], "please provide a valid model (elnet, gboost, rf or cnn)"
+    assert model in ["elnet", "gboost", "rf", "cnn"], "please provide a valid model (elnet, gboost, rf or cnn)"
     assert isinstance(pretrained, bool), "invalid pretrained, must be True/False"
     if pretrained:
         pass
     else:
-        assert len(X_train)==len(y_train), "X_train and y_train must be the same length"
+        assert len(X_train) == len(y_train), "X_train and y_train must be the same length"
 
-    if len(set(y_train))==2: #Checking whether classification or regression is needed
-        classification = True #machen wir das so, Problem mit fällen wo 2 Label ls negativ gewerted werden oder so
+    if len(set(y_train)) == 2:  # Checking whether classification or regression is needed
+        classification = True  # machen wir das so, Problem mit fällen wo 2 Label ls negativ gewerted werden oder so
 
-    #pretrained modelle reinladen
+    # loading pretrained models
     if pretrained:
+        if not os.path.exists("model_path"):
+            raise FileNotFoundError("File not found, please specify exact name and make sure the location is correct")
         if model == "elnet":
-            try:
-                rmodel = pickle.load(open(model_path, 'rb'))
-            except FileNotFoundError:
-              print("File not found, please specify exact name and make sure the location is correct")
+            rmodel = pickle.load(open(model_path, 'rb'))
         elif model == "gboost":
-            try:
-                rmodel = pickle.load(open(model_path, 'rb'))
-            except FileNotFoundError:
-              print("File not found, please specify exact name and make sure the location is correct")
+            rmodel = pickle.load(open(model_path, 'rb'))
+        elif model == "ebm":
+            rmodel = pickle.load(open(model_path, 'rb'))
         elif model == "rf":
-            try:
-                rmodel = pickle.load(open(model_path, 'rb'))
-            except FileNotFoundError:
-              print("File not found, please specify exact name and make sure the location is correct")
+            rmodel = pickle.load(open(model_path, 'rb'))
         else:
             try:
                 rmodel = load_model(model_path)
             except IOError:
-               print("File not found, please specify exact name and make sure the location is correct")
-               raise
+                print("File not found, please specify exact name and make sure the location is correct")
+                raise
 
-    else:     #modelle neu trainieren
+    else:  # training new models
         if model == "elnet":
             rmodel = model_elastic_net(X_train, y_train, classification, **kwargs)
-        
+
+        elif model == "ebm":
+            rmodel = EBMmi(X_train, y_train, classification=classification, **kwargs)
         elif model == "gboost":
-            pass
+            rmodel = GB(X_train, y_train, classification=True)
 
         elif model == "rf":
             pass
 
         else:
-            pass
-
+            rmodel = model_brainnet_cnn(X_train, y_train, **kwargs)
 
     return rmodel
+
