f037546d9f2b0dec4c81f3a24b0d914178930369
leoschaabner <73901213+leoschaabner@users.noreply.github.com>
2022-03-14 19:26:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='connectome',
     packages=find_packages(),
     version='0.1.0',
     description='Connectome Innolab',
-    author='Kai Becker',
+    author='Jana Gauß, Jonas Klingele, Kai Becker, Katharina J. Brenner, Leo Schaabner',
+    author_email='',
     license='',
 )
