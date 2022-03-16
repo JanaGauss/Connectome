e6ef3e25b55f96777fe587c39bb8851ada2b1889
leoschaabner <73901213+leoschaabner@users.noreply.github.com>
2022-03-16 22:31:31 +0100

    Update test_pipeline.py

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -6,6 +6,8 @@ from connectome.models.framework import model_framework
 from connectome.models.evaluation import model_evaluation
 from connectome.visualization.viz_framework import visualization_framework
 from connectome.models.brainnet_cnn import preprocess_test_data_for_cnn
+from tf.keras.optimizers import Nadam
+from tf.keras.losses import Hinge
 
 
 DIMS = (10, 25, 50)
@@ -87,8 +89,8 @@ class TestPipeline(unittest.TestCase):
                                     model_path=None)
             print(model_evaluation(model, X_test, y_test))
 
-            #viz = visualization_framework(model=model, X=X_test,
-            #                              y=y_test, viz_method="FI")
+            viz = visualization_framework(model=model, X=X_test,
+                                          y=y_test, viz_method="FI_only")
 
     def test_pipeline_gb(self):
 
@@ -125,8 +127,8 @@ class TestPipeline(unittest.TestCase):
                                     model_path=None)
             print(model_evaluation(model, X_test, y_test))
 
-            #viz = visualization_framework(model=model, X=X_test,
-            #                              y=y_test, viz_method="GFI")
+            viz = visualization_framework(model=model, X=X_test,
+                                          y=y_test, viz_method="FI")
 
    def test_pipeline_cnn(self):
 
@@ -166,7 +168,47 @@ class TestPipeline(unittest.TestCase):
             viz = visualization_framework(model=model, X=X_test, y=y_test,
                                           viz_method="feature_attribution", method='saliency', average=True,
                                           ordered=False)
+            
+    def test_pipeline_cnn_opt(self):
 
+        for value in path_dict.values():
+            df = preprocess_mat_files(
+                matlab_dir=value["matlab_dir"],
+                excel_path=value["excel_path"])
+
+            classification = True
+            columns_drop = ["ConnID", "Apoe", "subject_id"]
+            target = "target"
+            y_0 = [0]
+            y_1 = [1]
+            train_size = 0.8
+            seed = 1855
+            split = True
+
+            X_train, y_train, X_test, y_test = prepare_data(data=df,
+                                                            classification=classification,
+                                                            columns_drop=columns_drop,
+                                                            target=target, y_0=y_0,
+                                                            y_1=y_1,
+                                                            train_size=train_size,
+                                                            seed=seed, split=split)
+            model = model_framework(X_train=X_train,
+                                    y_train=y_train,
+                                    model="cnn",
+                                    pretrained=False,
+                                    model_path=None,
+                                    epochs=1,
+                                    patience=1,
+                                    optimizer=Nadam(),
+                                    loss=Hinge())
+
+            X_test, y_test = preprocess_test_data_for_cnn(X_test, y_test, aggregation=False, reorder=False)
+
+            print(model_evaluation(model, X_test, y_test))
+
+            viz = visualization_framework(model=model, X=X_test, y=y_test,
+                                          viz_method="feature_attribution", method='saliency', average=True,
+                                          ordered=False)
             
 if __name__ == '__main__':
     unittest.main()
